ae77207321ed80b0304cf8f20b0d8cdc0ecfdd12
Ritamnandy <nandyritam519@gmail.com>
2026-06-13 17:54:17 +0000

    feat: add type definitions for bcrypt and update ApiResponse data type

---
+++ server/src/constants.ts
@@ -0,0 +1,11 @@
+
+enum loginType
+{
+    EMAIL_PASSWORD = "EMAIL_PASSWORD",
+    GOOGLE = "GOOGLE",
+}
+
+const db_Name = "Todo_App"
+
+
+export { loginType, db_Name }
--- server/src/utils/apiresponse.ts
+++ server/src/utils/apiresponse.ts
@@ -3,11 +3,11 @@ class ApiResponse
 {
     private statusCode: number;
     private message: string;
-    private data: unknown;
+    private data: (string | object)[];
     private error: null;
     private success: boolean
 
-    constructor ( statusCode: number, message: string, data: unknown )
+    constructor ( statusCode: number, message: string, data: ( string | object )[] )
     {
         this.statusCode = statusCode;
         this.message = message;
