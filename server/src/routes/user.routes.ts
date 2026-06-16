a60f3e1da075298ef83c87b473e82a0e020fba27
Ritamnandy <nandyritam519@gmail.com>
2026-06-16 19:57:15 +0000

    feat: add user routes for registration, login, and Google authentication

---
+++ server/src/routes/user.routes.ts
@@ -0,0 +1,61 @@
+
+import "../passport/oauth.js";
+import express from "express";
+import { validate } from "../validators/validate.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { registerValidator, loginValidator } from "../validators/auth/user.validators.js";
+import
+{
+    registerUser,
+    resendVerificationCode,
+    verifyEmail,
+    loginUser,
+    logoutUser,
+    setAvatar,
+    forgetPassword,
+    refreshAccessToken,
+    socialLogin
+} from "../controllers/user.controllers.js"
+import passport from "passport";
+
+
+const router = express.Router()
+
+// google routes
+
+router.route( '/google' ).get(
+    passport.authenticate(
+        'google',
+        {
+            scope: [ 'email', 'profile' ]
+        }
+    )
+)
+
+router.route( '/google/callback' ).get(
+    passport.authenticate( 'google',
+        {
+            failureRedirect: '/login'
+        }, socialLogin
+    )
+)
+
+
+//public routes
+
+router.route( "/register" ).post( registerValidator(), validate, registerUser )
+router.route( "/resend-verification-code" ).post( resendVerificationCode )
+router.route( "/verify-email" ).post( verifyEmail )
+router.route( "/login" ).post( loginValidator(), validate, loginUser )
+router.route( "/refresh-access-token" ).post( refreshAccessToken )
+router.route( "/forget-password" ).post( forgetPassword )
+
+
+//secure routes
+
+router.route( "/logout" ).post( verifyJWT, logoutUser )
+router.route( "/set-avatar" ).post( verifyJWT, setAvatar )
+
+
+
+export default router
